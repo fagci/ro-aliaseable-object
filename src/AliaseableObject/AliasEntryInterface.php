8f47fae58fcad5ea9ac443d4dd0d56cefd8fb93a
Mikhail Yudin <yumih@r-o.ru>
2018-01-30 14:51:55 +0700

    some fixes

--- src/AliaseableObject/AliasEntryInterface.php
+++ src/AliaseableObject/AliasEntryInterface.php
@@ -26,7 +26,7 @@ interface AliasEntryInterface
     /**
      * @return AliasEntryTrait[]
      */
-    public function getDuplicates($alias = null);
+    public function getDuplicates();
 
     /**
      * @return null|AliaseableObjectInterface|AliaseableObjectTrait
